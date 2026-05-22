6f0d82ea65942e632e4ab48e006518df6e1421de
KevinRabbe <rabbekevin@icloud.com>
2026-05-22 07:48:58 +0200

    Extract hotkey help panel renderer

--- GodotClient/Scripts/RtsClientRoot.cs
+++ GodotClient/Scripts/RtsClientRoot.cs
@@ -855,16 +855,7 @@ public partial class RtsClientRoot : Node2D
 	private void DrawHotkeyHelpPanel(Vector2 uiOrigin, Vector2 uiSize)
 	{
 		GodotHotkeyHelpEntry[] entries = GodotHotkeyHelpBuilder.Build(researchIsWired: true);
-		Vector2 panelPos = uiOrigin + new Vector2(Mathf.Max(0.0f, uiSize.X - 620.0f - 12.0f), 264.0f);
-		float panelWidth = 620.0f;
-		float panelHeight = 24.0f + entries.Length * 16.0f + 12.0f;
-		DrawRect(new Rect2(panelPos, new Vector2(panelWidth, panelHeight)), new Color(0.0f, 0.0f, 0.0f, 0.56f));
-		DrawString(ThemeDB.FallbackFont, panelPos + new Vector2(10.0f, 20.0f), "Hotkeys (H/F11)", HorizontalAlignment.Left, -1.0f, 15, Colors.WhiteSmoke);
-		for (int i = 0; i < entries.Length; i++)
-		{
-			string line = entries[i].Input + ": " + entries[i].Action;
-			DrawString(ThemeDB.FallbackFont, panelPos + new Vector2(10.0f, 38.0f + i * 16.0f), line, HorizontalAlignment.Left, -1.0f, 13, Colors.LightGray);
-		}
+		RtsHotkeyHelpPanelRenderer.Draw(this, uiOrigin, uiSize, entries);
 	}
 
 	private void DrawSelectionRing(GodotPrimitiveDto primitive, Color color)
